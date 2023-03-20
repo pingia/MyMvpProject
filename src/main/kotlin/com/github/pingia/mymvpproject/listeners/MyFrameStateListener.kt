05dc9aef3b31815bae459710b26b37cf725f3f96
GitHub Action <action@github.com>
2023-03-20 02:24:02 +0000

    Template cleanup

--- src/main/kotlin/com/github/pingia/mymvpproject/listeners/MyFrameStateListener.kt
+++ src/main/kotlin/com/github/pingia/mymvpproject/listeners/MyFrameStateListener.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template.listeners
+package com.github.pingia.mymvpproject.listeners
 
 import com.intellij.ide.FrameStateListener
 import com.intellij.openapi.diagnostic.thisLogger
